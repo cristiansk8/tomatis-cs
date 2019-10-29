ae5f48d56134968c4784e0104548d44a5b8d4bbc
unknown <cdpsk8@gmail.com>
2019-10-29 18:00:51 -0500

    front child page porque se llama metodo tomatis

--- wp-content/themes/dep_tomatis/includes/metodo-tomatis/child-pages/metodo-tomatis.php
+++ wp-content/themes/dep_tomatis/includes/metodo-tomatis/child-pages/metodo-tomatis.php
@@ -1,18 +1,5 @@
-<div class="metodoEscuchar">
-<article>
- <img src="https://tomatis.com.co/wp-content/uploads/2019/10/Imagen-Metodo-Tomatis.png" alt=" Quijote">
- <span>En 1957, presenta <b>“Las Leyes Tomatis”</b> en la Academia de las Ciencias de París: </span>
-<ul>
-  <li type="disc">La voz solo reproduce lo que el oído escucha.</li>
-  <li type="disc">Si la escucha se modifica, también se modifica la voz inmediata e inconscientemente.</li>
-  <li type="disc">Una estimulación sonora sostenida durante un tiempo específico puede  transformar la voz de forma permanente</li>
-</ul>
-<div class="">
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-</div>
-En un lugar de la Mancha, de cuyo nombre no quiero acordarme, no ha mucho tiempo que vivía un hidalgo de los de lanza en astillero, adarga antigua, rocín flaco y galgo corredor. Una olla de algo más vaca que carnero, salpicón las más noches, duelos y quebrantos los sábados, lentejas los viernes, algún palomino de añadidura los domingos, consumían las tres partes de su hacienda. El resto della concluían sayo de velarte, calzas de velludo para las fiestas con sus pantuflos de lo mismo, los días de entre semana se honraba con su vellori de lo más fino. Tenía en su casa una ama que pasaba de los cuarenta, y una sobrina que no llegaba a los veinte, y un mozo de campo y plaza, que así ensillaba el rocín como tomaba la podadera. Frisaba la edad de nuestro hidalgo con los cincuenta años, era de complexión recia, seco de carnes, enjuto de rostro; gran madrugador y amigo de la caza. uieren decir que tenía el sobrenombre de Quijada o Quesada (que en esto hay alguna diferencia en los autores que deste caso escriben), aunque por conjeturas verosímiles se deja entender que se llama Quijana; pero esto importa poco a nuestro cuento; basta que en la narración dél no se salga un punto de la verdad.
+<div class="metodoTomatis">
 
-</article>
 <div class="block-1">
 <h2>¿Por qué se llama Método Tomatis?</h2>
 El nombre proviene de su creador el Otorrinolaringólogo e Investigador francés Alfred A. Tomatis (1920-2001). Sus investigaciones revolucionaron la comprensión del oído y su relación con los trastornos sensorio-motores, emocionales, cognitivos y comunicativos.
@@ -24,20 +11,23 @@ Su trabajo con cantantes y trabajadores de aviación que sufrían sordera profes
 <hr>
 
 </div>
-<article class="container">
-  <img class="forma" src="https://tomatis.com.co/wp-content/uploads/2019/10/Imagen-Metodo-Tomatis.png" alt="">
-  <span>En 1957, presenta <b>“Las Leyes Tomatis”</b> en la Academia de las Ciencias de París: </span>
- <ul>
-   <li type="disc">La voz solo reproduce lo que el oído escucha.</li>
-   <li type="disc">Si la escucha se modifica, también se modifica la voz inmediata e inconscientemente.</li>
-   <li type="disc">Una estimulación sonora sostenida durante un tiempo específico puede  transformar la voz de forma permanente</li>
- </ul>
- <div class="">
-   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+<article class="block-2">
+  <div class="block-1">
+    <img class="forma" src="https://tomatis.com.co/wp-content/uploads/2019/10/Imagen-Metodo-Tomatis.png" alt="">
+    <span>En 1957, presenta <b>“Las Leyes Tomatis”</b> en la Academia de las Ciencias de París: </span>
+   <ul>
+     <li type="disc">La voz solo reproduce lo que el oído escucha.</li>
+     <li type="disc">Si la escucha se modifica, también se modifica la voz inmediata e inconscientemente.</li>
+     <li type="disc">Una estimulación sonora sostenida durante un tiempo específico puede  transformar la voz de forma permanente</li>
+   </ul>
+  </div>
+ <div class="block-2--high">
+   <div class="block-2--high--text">
+     <p>La misión que se planteó este médico francés hace sesenta años fue <span>ayudar al mayor número de personas posible</span> con dificultades comunicativas, cognitivas, sensoriales, motoras, sociales y emocionales.</p>
+
+   </div>
  </div>
 
-
 </article>
-
 
 </div>
